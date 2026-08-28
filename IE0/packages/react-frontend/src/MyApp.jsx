089500177b90e02bcdea04f9304c93ff8225e05f
Migue654 <mmedin52@calpoly.edu>
2026-08-28 13:20:01 -0700

    Beginning of Part 10

---
+++ IE0/packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,34 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table"
+import { Tab } from "three/examples/jsm/inspector/ui/Tab.js";
+import { useState } from "react";
+import { update } from "three/examples/jsm/libs/tween.module.js";
+
+import Form from "./Form"
+
+
+
+function MyApp() {
+    const [characters, setcharacters] = useState( [])
+    function removeOneCharacter(index) {
+        const updated = characters.filter((character, i) => {
+        return i !== index;
+        });
+
+        setcharacters(updated)
+    }
+
+    function updatList (person){
+        setcharacters([...characters,person])
+    }
+
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form handleSubmit= {updatList}/>
+        </div>
+    )
+}
+
+export default MyApp;
